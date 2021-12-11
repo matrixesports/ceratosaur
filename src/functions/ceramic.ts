aba6cf6ee07bc6117016a238b565e59c23faebc7
LaJBel <lajbelms@gmail.com>
2021-12-11 20:41:23 -0300

    some misc changes, add types and publish beta 4

--- src/functions/ceramic.ts
+++ src/functions/ceramic.ts
@@ -1,6 +1,7 @@
 import { Ceramic } from "../types/mod.ts";
 import { createStream, getStream } from "./stream.ts";
 
+/** Ceramic function for easy-use all Ceramic methods without put the URL */
 export function ceramic(url: string): Ceramic {
 	return {
 		getStream: (id: string) => { return getStream(url + "/api/v0/streams", id) },
--- src/types/stream/state.ts
+++ src/types/stream/state.ts
@@ -1,7 +1,9 @@
 // deno-lint-ignore-file no-explicit-any
 
 export interface State {
+	/** State type */
+	type: number;
+	/** Content of the State */
 	content: any;
 	log: any;
-	type: number;
 }
